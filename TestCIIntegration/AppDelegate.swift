b85c150130fdcf4230d5c748d4b9d115fb9eea31
Alexander Kravchenko <alex.kravchenko@anoda.mobi>
2019-07-30 16:05:10 +0300

    Added Fabric

--- TestCIIntegration/AppDelegate.swift
+++ TestCIIntegration/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Crashlytics
+import Fabric
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        Fabric.with([Crashlytics.self])
         return true
     }
 
